4bd5ccbe7e19865b36f276c414c1ab6438f343b8
arqdsis <arqdsis@mocli10214.animaedu.intranet>
2018-03-13 19:53:36 -0300

    Arquitetura de Software

---
+++ arqsw_sdesk_a01/src/br/usjt/arqsw/service/FilaService.java
@@ -0,0 +1,27 @@
+package br.usjt.arqsw.service;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import br.usjt.arqsw.dao.FilaDAO;
+import br.usjt.arqsw.entity.Fila;
+
+/**
+ * 
+ * @author Lucas Ribeiro Rios - 816114323
+ */
+
+public class FilaService {
+	private FilaDAO dao;
+	
+	public FilaService() {
+		dao = new FilaDAO();
+	}
+	public ArrayList<Fila> listarFilas() throws IOException{
+		return dao.listarFilas();
+	}
+	
+	public Fila carregar(int id) throws IOException{
+		return dao.carregar(id);
+	}
+}
